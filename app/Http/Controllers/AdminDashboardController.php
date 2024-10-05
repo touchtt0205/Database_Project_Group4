8b41bb68508f8e55a376a87699c7289101013a6b
SUPATKN <yacht1400@gmail.com>
2024-10-05 16:58:01 +0700

    admin dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -13,9 +13,17 @@ class AdminDashboardController extends Controller
 
     public function index()
     {
+        // ดึงข้อมูลจำนวนผู้ใช้
+        $userCount = User::count();
+
+        // ดึงข้อมูลยอดรวมที่ใช้ในการซื้อขาย
+        $totalSpent = OrderHistory::sum('price');
+
+        return view('admin.dashboard', compact('userCount', 'totalSpent'));
         if (!Auth::user()->isAdmin) {
             return redirect('/dashboard'); // redirect หากไม่ใช่ admin
         }
+
         // แสดงหน้า dashboard สำหรับ admin
         return view('admin.dashboard');
     }
