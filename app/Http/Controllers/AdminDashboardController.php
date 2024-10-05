54524343c6246f7e65111410e13f5a12adf46baa
SUPATKN <yacht1400@gmail.com>
2024-10-05 15:30:35 +0700

    protect admin route

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
 {
+    
+
     public function index()
     {
+        if (!Auth::user()->isAdmin) {
+            return redirect('/dashboard'); // redirect หากไม่ใช่ admin
+        }
+
+        // แสดงหน้า dashboard สำหรับ admin
         return view('admin.dashboard');
     }
 }
